45d8ef06c39563fa144a98f25c747652d7fba54f
Davy ROBERT <davyrobert90@gmail.com>
2023-01-31 21:17:20 +0100

    update accueil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Accueil from "./pages/Accueil";
-import Inscription from "./pages/Inscription";
-import Connexion from "./pages/Connexion";
+import Inscription from "./components/Inscription";
+import Connexion from "./components/Connexion";
+import PageNotFound from "./pages/PageNotFound";
+import MyCollection from "./pages/MyCollection";
+import AllCollections from "./pages/AllCollections";
+// import Tendances from "./pages/Tendances";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
           <Route path="/" element={<Accueil />} />
           <Route path="/connexion" element={<Connexion />} />
           <Route path="/inscription" element={<Inscription />} />
+          <Route path="/mycollection" element={<MyCollection />} />
+          <Route path="/allcollections" element={<AllCollections />} />
+          {/* <Route path="/tendances" element={<Tendances />} /> */}
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+// import homePicture from "../assets/homePicture.jpg";
+import "../styles/Accueil.css";
 
 export default function Accueil() {
   return (
-    <div>
-      <header className="App-header ">
-        <p className="text-3xl text-center">Go Checkpoint 4</p>
+    <div className="accueilPage">
+      <header className="App-header">
+        {/* <img className="" src={homePicture} alt="homePicture" /> */}
+        <div className="flex justify-center pt-12">
+          <p className="text-6xl p-5 text-center text-white border-solid inline-block border-2 border-yellow-600">
+            COINS Drop
+          </p>
+        </div>
+        <div className="text-white h-72 w-8/12 lg:w-5/12 flex justify-start ">
+          <p className="p-5 ml-10 pt-32 md:text-xl text-yellow-500">
+            Bienvenue sur notre site dédié aux collectionneurs de pièces de
+            monnaie rares. <br />
+            <br />
+            <span className="text-white">
+              Nous sommes ravis de vous présenter notre sélection de pièces
+              uniques et précieuses provenant du monde entier.
+              <br />
+              <br />
+              Chaque pièce a une histoire unique à raconter.
+              <br />
+              <br />
+              Notre site est conçu pour être facile à utiliser et vous permet de
+              trouver rapidement la pièce que vous recherchez.
+            </span>
+          </p>
+        </div>
+        {/* <div className=""></div> */}
       </header>
       <main>
-        <div className="flex justify-end">
+        <div className="flex justify-center pb-6 pt-6">
           <NavLink to="/connexion">
             <button
               type="button"
-              className="text-2xl text-center inline bg-slate-100 hover:scale-110 border-black border-solid border-2 rounded-2xl"
+              className="text-2xl text-center inline bg-slate-100 hover:scale-110 opacity-70 hover:opacity-100 border-black border-solid border-2 rounded-xl p-2"
             >
               Connexion{" "}
             </button>
@@ -20,7 +46,7 @@ export default function Accueil() {
           <NavLink to="/inscription">
             <button
               type="button"
-              className="text-2xl text-center inline bg-slate-100 hover:scale-110 ml-5 mr-5 border-black border-2 rounded-2xl"
+              className="text-2xl text-center inline bg-slate-100 hover:scale-110 opacity-70 hover:opacity-100 ml-5 mr-5 border-black border-2 rounded-xl p-2"
             >
               Inscription{" "}
             </button>
